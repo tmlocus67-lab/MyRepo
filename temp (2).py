86ecda0a9c8d825ba92a2704055f96ea0d84687b
Miles Somerfield Green <msomerfieldgreen@fusionacademy.com>
2026-06-30 15:51:51 -0700

    or agu

--- temp (2).py
+++ temp (2).py
@@ -1,11 +1,1 @@
-
-
-
-
-try:
-    number = int(input("Enter a number: "))
-    result = 10 / number
-except Exception as e:
-    print(f"Some error occurred: {e}")
-else:
-    print("no errors")
+ff
