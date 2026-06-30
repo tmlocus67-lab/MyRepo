b556d393b104c1770c9cba9d89e117afc1ac8e8b
Miles Somerfield Green <msomerfieldgreen@fusionacademy.com>
2026-06-30 15:31:58 -0700

    on new

--- temp (2).py
+++ temp (2).py
@@ -4,18 +4,4 @@
 
 try:
     number = int(input("Enter a number: "))
-    result = 10 / number
-except Exception as e:
-    print(f"Some error occurred: {e}")
-else:
-    print("no errors")
-
-
-
-    fffffdddddddddd
-    fff
-    fddaa
-
-
-
-    new new
+    r
